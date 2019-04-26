dad32299591664fcf7bb1a893f3b58c0a95ff9ba
dosdrtt <lnpj.office@icloud.com>
2019-04-27 07:53:15 +0800

    init 0_1_1_1_1_1

---
+++ GunNetty-Plugs/README.md
@@ -0,0 +1,1 @@
+# GunNetty-Plugs
---
+++ GunNetty-Utils/src/top/gunplan/utils/GunBytesUtil.java
@@ -0,0 +1,141 @@
+package top.gunplan.utils;
+
+import com.sun.istack.internal.NotNull;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author dosdrtt
+ */
+public final class GunBytesUtil {
+    public static class GunWriteByteUtil {
+        public GunWriteByteUtil(byte[] input) {
+            this.input = input;
+        }
+
+        int nowflag = 0;
+        byte[] input;
+
+        public void writeByte(byte bin) {
+            input[nowflag++] = bin;
+        }
+
+        public void write(byte[] save) {
+            write(save.length, save);
+        }
+
+        public void write(int len, byte[] save) {
+            System.arraycopy(save, 0, input, nowflag, len);
+            nowflag += len;
+        }
+
+        public void write(String save) {
+            byte[] bsave = save.getBytes();
+            write(save.length(), bsave);
+        }
+
+        public void write(int save) {
+            writeByte((byte) (save >> 8));
+            writeByte((byte) save);
+        }
+
+        public void write64(int save) {
+            write(save >> 16);
+            write(save);
+        }
+    }
+
+
+    public static class GunReadByteUtil {
+        public GunReadByteUtil(byte[] input) {
+            this.output = input;
+        }
+
+        int nowflag = 0;
+        byte[] output;
+
+        public byte readByte() {
+            return output[nowflag++];
+        }
+
+        public int readInt() {
+            byte[] read = readByte(2);
+            int res;
+            res = read[0];
+            res = res << 8;
+            res = res ^ read[1];
+            return res;
+        }
+
+        public int readInt64() {
+            int left = readInt();
+            int right = readInt();
+            return (left << 16) ^ right;
+        }
+
+        public byte[] readByte(int len) {
+            byte[] op = new byte[len];
+            System.arraycopy(output, nowflag, op, 0, len);
+            nowflag += len;
+            return op;
+        }
+    }
+
+    private static byte[] incrementCopy(byte[] oldbytes, int increment) {
+        byte[] newbbytes = new byte[oldbytes.length + increment];
+        System.arraycopy(oldbytes, 0, newbbytes, 0, oldbytes.length);
+        return newbbytes;
+    }
+
+
+    public static boolean compareBytesFromStart(final byte[] src, @NotNull final byte... b) {
+        for (int i = 0; i < b.length; i++) {
+            if (src[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean compareBytesFromEnd(final byte[] src, @NotNull final byte... b) {
+        for (int i = b.length - 1; i > 0; i--) {
+            if (src[src.length - i] != b[b.length - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static ByteBuffer heapbuff;
+
+    public static void init(int size) {
+        heapbuff = ByteBuffer.allocate(size);
+    }
+
+    public static byte[] readFromChannel(SocketChannel channel, int increment) throws IOException {
+        byte[] save = new byte[increment];
+        int nowpoint = 0;
+        int maxsize = increment;
+        int readlen;
+        while ((readlen = channel.read(heapbuff)) > 0) {
+            byte[] buffer = heapbuff.array();
+            if (maxsize - nowpoint < buffer.length) {
+                save = GunBytesUtil.incrementCopy(save, increment);
+                maxsize += increment;
+            }
+            System.arraycopy(buffer, 0, save, nowpoint, buffer.length);
+            nowpoint += readlen;
+            heapbuff.clear();
+        }
+        byte[] realsave = new byte[nowpoint];
+        System.arraycopy(save, 0, realsave, 0, nowpoint);
+        return nowpoint != 0 ? realsave : null;
+
+    }
+
+    public static byte[] readFromChannel(SocketChannel channel) throws IOException {
+        return readFromChannel(channel, 512);
+    }
+}
