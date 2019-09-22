7279e8edf326eb36a864d721826d38d863760326
frank <lnpj.office@icloud.com>
2019-09-22 08:34:33 +0800

    test all push

--- GunNetty-Utils/src/top/gunplan/utils/lock/GunSync.java
+++
@@ -1,9 +0,0 @@
-package top.gunplan.utils.lock;
-
-import com.sun.corba.se.impl.orbutil.concurrent.Sync;
-
-public interface GunSync {
-    void acquire(int value) throws InterruptedException;
-
-    void release(int value);
-}
